79f7d709600f538034a36636b82f3a927c56c8a4
Luis Fer <pastillalc@gmail.com>
2019-12-08 23:03:21 -0400

    Id's for Layouts

--- app/src/main/java/com/example/kenkogym/LoginActivity.java
+++ app/src/main/java/com/example/kenkogym/LoginActivity.java
@@ -11,4 +11,35 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
 }

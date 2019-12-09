40b286902ec2adc319ddb68b97e2159377116590
x4tkc <tovilaskevin@gmail.com>
2019-12-09 13:00:35 -0400

    models User, Days, Exercise enum days exercise user

--- app/src/main/java/com/example/kenkogym/models/Repository/Days.java
+++ app/src/main/java/com/example/kenkogym/models/Repository/Days.java
@@ -1,4 +1,11 @@
 package com.example.kenkogym.models.Repository;
 
+import com.example.kenkogym.models.types.enumDays;
+
+import java.util.ArrayList;
+
 public class Days {
+    private enumDays day;
+    private String name;
+    private ArrayList<Exercise> list;
 }
--- app/src/main/java/com/example/kenkogym/models/Repository/Exercise.java
+++ app/src/main/java/com/example/kenkogym/models/Repository/Exercise.java
@@ -6,5 +6,10 @@ public class Exercise {
     private String name;
     private enumExercise type;
     private int image;
+    public Exercise (String name, enumExercise type, int image){
+        this.name=name;
+        this.type=type;
+        this.image=image;
+    }
 
 }

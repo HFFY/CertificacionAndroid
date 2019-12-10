450371911d67b1adac76bc2a48d40f6ab4ff738a
x4tkc <tovilaskevin@gmail.com>
2019-12-10 09:02:19 -0400

    repository users commit

--- app/src/main/java/com/example/kenkogym/studentsList/StudentsListRepository.java
+++ app/src/main/java/com/example/kenkogym/studentsList/StudentsListRepository.java
@@ -1,4 +1,36 @@
 package com.example.kenkogym.studentsList;
 
-public class StudentsListRepository {
+import androidx.lifecycle.LiveData;
+
+import com.example.kenkogym.utils.FireBaseRepository;
+import com.example.kenkogym.utils.models.Base;
+
+public class StudentsListRepository implements RepositoryUserImpl {
+
+    private static StudentsListRepository instance;
+
+    public static StudentsListRepository getInstance() {
+        if (instance == null) {
+            instance = new StudentsListRepository();
+        }
+        return instance;
+    }
+
+    private StudentsListRepository() {
+    }
+
+    @Override
+    public LiveData<Base> login(String email, String password) {
+        return FireBaseRepository.getInstance().login(email, password);
+    }
+
+    @Override
+    public LiveData<Base> getUser() {
+        return ApiRepository.getInstance().getRoutes();
+    }
+
+    @Override
+    public LiveData<Base> getUserDetails(String uuid) {
+        return null;
+    }
 }

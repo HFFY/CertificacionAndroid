7784b739d968e976a2fc7010b80c19eadda5f677
Hugo Flores <huflores@bnb.com.bo>
2019-12-18 20:35:40 -0400

    Nueva pantalla

---
+++ app/src/main/java/com/example/kenkogym/trainer/view/TrainerActivity.java
@@ -0,0 +1,16 @@
+package com.example.kenkogym.trainer.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.kenkogym.R;
+
+public class TrainerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trainer);
+    }
+}
